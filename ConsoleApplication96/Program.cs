f46b45225dd3cfa5ede0344995045cd23846dfa4
rasha nedam <rasha.system1983@gmail.com>
2024-10-16 22:35:17 +0300

    Initial commit

---
+++ ConsoleApplication96/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication96
+{
+    class Student
+    {
+        private string name;
+        private string address;
+        private double mark;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        public double Mark
+        {
+            get { return mark; }
+            set { mark = value; }
+        }
+
+        public Student(string n, string addr, double m)
+        {
+            name = n;
+            address = addr;
+            mark = m;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Address: " + address);
+            Console.WriteLine("Mark: " + mark);
+
+        }
+
+        class program
+        {
+            static void Main(string[] args)
+            {
+                Student student = new Student("rasha", "60 St", 75.5);
+
+                student.Display();
+                Console.ReadLine();
+
+
+
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# ConsoleApplication96
+ 
